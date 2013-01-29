c2f944017e76657b7812f2a467d27bfb3e23a8b5
Harri Salokorpi <hsalokor@iki.fi>
2013-01-30 00:45:32 +0200

    Initial failing spec for throws? checker
    It is currently implemented as macro, but I need to see whether I can
    get it to work.

---
+++ src/cljs/litmus/assert/macros.clj
@@ -0,0 +1,10 @@
+(ns litmus.assert.macros)
+
+(defmacro throws? [body arrow exception regex]
+  "Create necessary structure for checking exception throws without too much
+   syntactic noise.
+
+   Note: the aget is required to bypass Google Closure compiler behaviour, where
+   property with reserved keyword name causes compilation failure (in this case,
+   'throws' or 'throw')."
+  `(apply (aget js/chai.assert "throws") [(fn [] ~@body) ~exception ~regex]))
--- test/cljs/litmus/test/assert.cljs
+++ test/cljs/litmus/test/assert.cljs
@@ -1,8 +1,12 @@
 (ns litmus.test.assert
   (:use [litmus.assert :only [equals? =>]])
-  (:use-macros [litmus.macros :only [describe given then]]))
+  (:use-macros [litmus.macros :only [describe given then]]
+               [litmus.assert.macros :only [throws?]]))
 
 (describe "Assert (equals? actual => expected)"
           (given "function (+ 2 2)"
                  (then "it returns 4"
-                       (equals? (+ 2 2) => 4))))
+                       (equals? (+ 2 2) => 4)))
+          (given "invalid form"
+                 (then "it throws exception"
+                       (throws? (equals? (+ 2 2) 4) => js/Error #"Invalid.*=>.*"))))
