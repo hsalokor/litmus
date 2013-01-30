c0d6a8706b69425c04778178e62f68a38915b3e9
Harri Salokorpi <hsalokor@iki.fi>
2013-01-30 22:41:29 +0200

    Set version to 0.0.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject litmus "0.1.0-SNAPSHOT"
+(defproject litmus "0.0.1"
   :description "Testing library for Clojurescript"
   :url "http://github.com/hsalokor/litmus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "0.3.0-1"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:test-commands {"unit" ["mocha-phantomjs" "resources/test.html"]}
               :builds {:dev {:source-paths ["src/cljs"]
@@ -14,6 +14,7 @@
                                                   "src/cljs/externs/chai.js"]
                                         :pretty-print true}}
                        :prod {:source-paths ["src/cljs"]
+			      :jar true
                               :compiler {:output-to "target/litmus.js"
                                          :optimizations :advanced
                                          :externs ["src/cljs/externs/mocha.js"
