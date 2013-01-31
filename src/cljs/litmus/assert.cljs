3af4a8a24eff18ce3e5c579fe1274c6f6b8b19ed
Harri Salokorpi <hsalokor@iki.fi>
2013-01-31 08:34:30 +0200

    Add docstring for (equals? ...)

--- src/cljs/litmus/assert.cljs
+++ src/cljs/litmus/assert.cljs
@@ -4,6 +4,10 @@
 (def => "=>")
 
 (defn equals?
+  "Checks that two values are equal. Requires arrow form,
+   throws exception if => is not provided.
+
+   Example: (equals? (+ 2 2) => 4)"
   [expected arrow actual]
   (check-arrow arrow)
   (.equal chai.assert expected actual))
