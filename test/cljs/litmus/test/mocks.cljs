d2578eeeb809b33c65105df5bb17e19ce2b65fd3
Harri Salokorpi <hsalokor@iki.fi>
2013-02-03 18:12:52 +0200

    Fix test to use (provided ...) syntax

--- test/cljs/litmus/test/mocks.cljs
+++ test/cljs/litmus/test/mocks.cljs
@@ -1,7 +1,7 @@
 (ns litmus.test.mocks
   (:use [litmus.assert :only [=> equals?]])
   (:use-macros [litmus.macros :only [describe given then]]
-               [litmus.mocks.macros :only [with-mocks]]))
+               [litmus.mocks.macros :only [provided]]))
 
 (defn test-fn [number]
   (+ 5 number))
@@ -9,5 +9,6 @@
 (describe "A mock"
           (given "installed with with-mocks macro"
                  (then "verifies that function is called"
-                       (with-mocks [(test-fn 1) => 2]
+                       (provided [(test-fn 1 => 2)
+                                  (test-fn 2 => 4)]
                          (equals? (test-fn 1) => 2)))))
