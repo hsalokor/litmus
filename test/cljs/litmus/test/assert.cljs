414295ac0192b5bb884ade6879429f616e34e554
Harri Salokorpi <hsalokor@iki.fi>
2013-02-12 00:51:32 +0200

    Expand tests to cover not-throws? and add crosschecks for exception asserts

--- test/cljs/litmus/test/assert.cljs
+++ test/cljs/litmus/test/assert.cljs
@@ -1,11 +1,15 @@
 (ns litmus.test.assert
   (:use [litmus.assert :only [=> equals? not-equals? ok?]])
   (:use-macros [litmus.macros :only [describe given then]]
-               [litmus.assert.macros :only [throws?]]))
+               [litmus.assert.macros :only [throws? not-throws?]]))
 
 (defn throw-exception [] (throw (js/Error. "Error occured")))
 
 (describe "Assert (throws? (.. form ..) => Exception #\"regexp\")"
+          (given "function not throwing exception"
+                 (then "fails the check"
+                       (throws? (throws? (+ 1 1) => js/Error #"lol")
+                                => js/Error #"expected.*to throw.*")))
           (given "function throwing exception"
                  (then "exception is correctly handled"
                        (throws? (throw-exception) => js/Error #"Error occured")
@@ -13,7 +17,20 @@
                                 => js/Error #"Error occured")))
           (given "invalid form"
                  (then "it throws exception"
-                       (throws? (throws? (+ 2 2 ) 1 2 "invalid")
+                       (throws? (throws? (+ 2 2 ) 2 "invalid")
+                                => js/Error #"Invalid.*=>.*"))))
+
+(describe "Assert (not-throws? (.. form ..) => Exception)"
+          (given "function not throwing exception"
+                 (then "exception check passes"
+                       (not-throws? (+ 1 1) => js/Error)))
+          (given "function throwing exception"
+                 (then "fails the check"
+                       (throws? (not-throws? (throw (js/Error. "Error occured")) => js/Error)
+                                => js/Error #"expected.*to not throw.*")))
+          (given "invalid form"
+                 (then "it throws exception"
+                       (throws? (not-throws? (+ 2 2 ) 2 "invalid")
                                 => js/Error #"Invalid.*=>.*"))))
 
 (describe "Assert (equals? actual => expected)"
