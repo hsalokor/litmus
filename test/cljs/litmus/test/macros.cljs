110d30a8891186159bec7a5f2bec06faae22ad11
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2013-08-06 16:39:25 +0300

    Add tests for before and after

--- test/cljs/litmus/test/macros.cljs
+++ test/cljs/litmus/test/macros.cljs
@@ -1,6 +1,6 @@
 (ns litmus.test.macros
   (:use [litmus.assert :only [=> equals?]])
-  (:use-macros [litmus.macros :only [describe given then it]]))
+  (:use-macros [litmus.macros :only [describe given then it before after]]))
 
 (describe "Test form"
 
@@ -21,3 +21,30 @@
                  (it "executes multiple assertions inside"
                        (equals? 1 => 1)
                        (equals? 2 => 2))))
+
+(def before-test-atom (atom 0))
+
+(describe "Before hook"
+          (before (swap! before-test-atom inc))
+          (given "when set up in describe block"
+                 (then "is triggered before test"
+                       (equals? @before-test-atom => 1))
+                 (then "is triggered only once"
+                       (equals? @before-test-atom => 1))))
+
+(def after-test-atom (atom 0))
+
+(describe "After hook"
+          (describe "in first describe"
+                 (after (swap! after-test-atom inc))
+                 (it "is not triggered before exit"
+                       (equals? @after-test-atom => 0)))
+          (describe "in second describe"
+                 (after (swap! after-test-atom inc))
+                 (it "is triggered before second test (after exiting first test)"
+                     (equals? @after-test-atom => 1))
+                 (it "<dummy test>"
+                     (equals? 1 => 1)))
+          (describe "In third describe"
+                 (it "the previous describe block has triggered only 1 after"
+                       (equals? @after-test-atom => 2))))
