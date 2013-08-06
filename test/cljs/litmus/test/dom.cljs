b422294c02046f1520ccdf1109dcc289a0a708dd
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2013-08-06 19:55:23 +0300

    Extend dom.cljs tests for exists? assertion

--- test/cljs/litmus/test/dom.cljs
+++ test/cljs/litmus/test/dom.cljs
@@ -4,7 +4,16 @@
   (:use-macros [litmus.macros :only [describe given then]]
                [litmus.assert.macros :only [throws? not-throws?]]))
 
-(describe "Exists? check"
-          (given "for existing element #my-node"
-                 (then "it passes exists? check for selector #my-node"
-                       (dom/exists? "#my-node" => true))) )
+(describe "DOM assertions"
+          (given "calling exists? for existing element #my-node"
+                 (then "it passes check for selector #my-node if result should be true"
+                       (dom/exists? "#my-node" => true))
+                 (then "it throws assertion failure for selector #my-node if result should be false"
+                       (throws? (dom/exists? "#my-node" => false)
+                                => js/Error #".*Expected selector #my-node to match no elements.*")))
+          (given "calling exists? for non-existing #not-here node"
+                 (then "it throws assertion failure for selector #not-here if result should be true"
+                       (throws? (dom/exists? "#not-here" => true)
+                                => js/Error #".*Expected selector #not-here to match at least one element.*"))
+                 (then "it passes check for selector #not-found if result should be false"
+                       (dom/exists? "#not-here" => false))))
