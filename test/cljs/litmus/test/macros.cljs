58e4ba450b9ac3207ef15df0b82ba1e459401c82
Harri Salokorpi <hsalokor@iki.fi>
2013-08-15 15:38:48 +0300

    Introduce tests for before! and after!

--- test/cljs/litmus/test/macros.cljs
+++ test/cljs/litmus/test/macros.cljs
@@ -1,9 +1,12 @@
 (ns litmus.test.macros
-  (:use [litmus.assert :only [=> equals?]])
-  (:use-macros [litmus.macros :only [describe given then it before after before-each after-each]]))
+  (:use [jayq.util :only [wait]]
+        [litmus.assert :only [=> equals?]])
+  (:use-macros [litmus.macros :only [describe given then it before before! after after! before-each after-each]]))
 
 (def before-test-atom (atom 0))
+(def before!-test-atom (atom 0))
 (def after-test-atom (atom 0))
+(def after!-test-atom (atom 0))
 (def before-each-atom (atom 0))
 (def after-each-atom (atom 0))
 
@@ -33,6 +36,16 @@
     (then "is triggered only once"
       (equals? @before-test-atom => 1))))
 
+(describe "Asynchronous before! hook"
+  (before! on-ready
+    (wait 150 (fn [] (swap! before!-test-atom inc)
+                    (on-ready))))
+  (given "when set up in describe block"
+    (then "is triggered before test"
+      (equals? @before!-test-atom => 1))
+    (then "is triggered only once"
+      (equals? @before!-test-atom => 1))))
+
 (describe "After hook"
   (describe "in first describe"
     (after (swap! after-test-atom inc))
@@ -47,6 +60,25 @@
   (describe "In third describe"
     (it "the previous describe block with 2 tests has triggered after-hook once"
       (equals? @after-test-atom => 2))))
+
+(describe "Asynchronous after! hook"
+  (describe "in first describe"
+    (after! on-ready
+      (wait 150 (fn [] (swap! after!-test-atom inc)
+                      (on-ready))))
+    (it "is not triggered before exit"
+      (equals? @after!-test-atom => 0)))
+  (describe "in second describe"
+    (after! on-ready
+      (wait 150 (fn [] (swap! after!-test-atom inc)
+                      (on-ready))))
+    (it "is triggered before second test (after exiting first test)"
+      (equals? @after!-test-atom => 1))
+    (it "<dummy test>"
+      (equals? 1 => 1)))
+  (describe "In third describe"
+    (it "the previous describe block with 2 tests has triggered after-hook once"
+      (equals? @after!-test-atom => 2))))
 
 (describe "Before-each hook"
   (before-each (swap! before-each-atom inc))
