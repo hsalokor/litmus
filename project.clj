fe56bb4011a7446b97c50e1fab26f0699c70bac0
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2013-10-01 08:34:24 +0300

    Bump version to 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject litmus "0.4.0"
+(defproject litmus "0.4.1"
   :description "Testing library for Clojurescript"
   :url "http://github.com/hsalokor/litmus"
   :license {:name "Eclipse Public License"
